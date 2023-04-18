3343ac3944261ba97cad93ec06be8d75a952af42
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2023-04-18 23:19:25 +0200

    Delete uelsess line and

--- charge-control.sh
+++ charge-control.sh
@@ -6,9 +6,8 @@ CHARGING_STOP=75
 CHARGING_START=70
 SLEEP_DELAY=60
 CHARGESPEED=500000 #velocidad en mA * 1000
-CHARGINGSTATE=true
-NIGHT_TIME="23"
-DAY_TIME="07"
+NIGHT_TIME="2300" #Hora en formato HHMM
+DAY_TIME="0530"
 
 CURRENT_SWITCH=/sys/class/power_supply/battery/constant_charge_current_max
 CHARGING_SWITCH=/sys/class/power_supply/battery/input_suspend
@@ -45,26 +44,21 @@ nb_sleep_reset()
 
 while true
 do
-    HOUR=$(date +"%H")
-    if [ "$HOUR" -ge $NIGHT_TIME ] || [ "$HOUR" -lt $DAY_TIME ]; then
+    HOUR=$(date +"%H%M")
+    if [ "$HOUR" -ge $NIGHT_TIME ] || [ "$HOUR" -le $DAY_TIME ]; then
         CHARGING_START=40
-        CHARGING_STOP=55
-        echo "night"
+        CHARGING_STOP=60
     else
         CHARGING_START=70
         CHARGING_STOP=75
-        echo "day"
     fi
 
-    echo $CHARGING_MODE
-
-    if [ "$CHARGINGSTATE" = true ] && [ "$(cat $CHARGE_LEVEL)" -ge $CHARGING_STOP ]; then
+    CHARGINGSTATE=$(cat /sys/class/power_supply/battery/status)
+    if [ "$CHARGINGSTATE" = "Charging" ] && [ "$(cat $CHARGE_LEVEL)" -ge $CHARGING_STOP ]; then
         echo 1 > $CHARGING_SWITCH
-        CHARGINGSTATE=false
 
-    elif [ "$CHARGINGSTATE" = false ] && [ "$(cat $CHARGE_LEVEL)" -le $CHARGING_START ]; then
+    elif [ "$CHARGINGSTATE" = "Discharging" ] && [ "$(cat $CHARGE_LEVEL)" -le $CHARGING_START ]; then
         echo 0 > $CHARGING_SWITCH
-        CHARGINGSTATE=true
     fi
     nb_sleep $SLEEP_DELAY
 done
